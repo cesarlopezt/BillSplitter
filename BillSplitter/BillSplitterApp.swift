b56abd324b6339e6660a0149190c99c62b911a9c
cesarlopezt <cesarlopez2808@gmail.com>
2023-03-02 17:28:26 -0600

    Initial Commit

---
+++ BillSplitter/BillSplitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BillSplitterApp.swift
+//  BillSplitter
+//
+//  Created by Cesar Lopez on 3/2/23.
+//
+
+import SwiftUI
+
+@main
+struct BillSplitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
