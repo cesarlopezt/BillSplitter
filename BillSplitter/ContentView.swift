c0a8066daa3c4ae065226409d75b3eec69ffb337
cesarlopezt <cesarlopez2808@gmail.com>
2023-03-03 15:42:15 -0600

    Add layout and totalPerPerson calculation

--- BillSplitter/ContentView.swift
+++ BillSplitter/ContentView.swift
@@ -8,15 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeopleIndex = 0
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [0, 10, 15, 20, 25]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeopleIndex + 2)
+        let tipSelection = Double(tipPercentage)
+        let tipAmount = tipSelection / 100 * checkAmount
+        return (checkAmount + tipAmount) / peopleCount
     }
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number of People", selection: $numberOfPeopleIndex) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }.pickerStyle(.navigationLink)
+                }
+                
+                Section {
+                    Picker("Tip Percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }.pickerStyle(.segmented)
+                } header: {
+                    Text("How much tip you want to leave?")
+                }
+              
+                Section {
+                    Text(totalPerPerson,
+                         format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+            }
+            .navigationTitle("Bill Splitter")
+        }
+    }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
